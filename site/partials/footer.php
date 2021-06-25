b109eb7c3f98f3e1c98d9b7d5e11567edf0c418b
Rafael Gomes <faelfg@gmail.com>
2021-06-25 01:24:28 -0300

    Removing unecessary docker structure

--- site/partials/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-    <p>
-        Desenvolvido por Rafael Gomes <br>
-        Github: <a href="https://github.com/rafaelfgomes" target="_blank">rafaelfgomes</a> <br>
-        Repositório deste projeto: <a href="https://github.com/rafaelfgomes/teste-php-bling" target="_blank">Teste Bling</a>
-    </p>
-</div>
