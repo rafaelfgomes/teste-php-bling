3397cb0955a8683daabaf35150a48bf73d1803b6
Rafael Gomes <faelfg@gmail.com>
2021-06-28 22:39:56 -0300

    Finish test 5

--- testes/teste5.php
+++ testes/teste5.php
@@ -3,3 +3,59 @@
  -->
 
 <?php
+
+$text = 'The quick brown fox jumps over the lazy dog';
+$found = $partial = false;
+$searchText = '';
+$count = 0; 
+$explodedText = $explodedSearch = $check = [];
+
+echo 'Texto: ' . $text . "\n\n";
+
+echo 'Digite o texto que deseja procurar: ';
+$searchText = trim(fgets(STDIN, 1024));
+
+$explodedText = explode(' ', $text);
+
+if (preg_match('/^\S.*\s.*\S$/', $searchText)) {
+    $explodedSearch = explode(' ', $searchText);
+}
+
+if (!empty($explodedSearch)) {
+    foreach ($explodedSearch as $key => $searchWord) {
+        foreach ($explodedText as $word) {
+            if ($searchWord === $word) {
+                $found = true;
+                $count++;
+            }
+        }
+
+        if ($count < ($key + 1)) {
+            $partial = true;
+        }
+    }
+
+    if ($found && !$partial) {
+        echo 'Busca por \'' . $searchText . '\' encontrou o texto na frase: ' . $text . "\n\n";
+    } elseif ($found && $partial) {
+        echo 'Busca por \'' . $searchText . '\' econtrou parcialmente o texto na frase: ' . $text . "\n\n";
+    } else {
+        echo 'Texto \'' . $searchText . '\' não encontrado' . "\n\n";
+    }
+
+} else {
+
+    foreach ($explodedText as $key => $word) {
+            
+        if ($searchText === $word) {
+            $found = true;
+        }
+
+    }
+
+    if ($found) {
+        echo 'Texto \'' . $searchText . '\' encontrado na frase: ' . $text . "\n\n";
+    } else {
+        echo 'Texto \'' . $searchText . '\' não encontrado' . "\n\n";
+    } 
+}
